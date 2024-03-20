d23d308fdd4e7938a9c795c0830b5c6b7515f93a
Jessica Knott <knottfam@jessicas-imac.mynetworksettings.com>
2024-03-20 10:49:19 -0400

    origin

---
+++ index.js
@@ -0,0 +1,19 @@
+const freelancers = [
+  {
+    name: `Alice`,
+    occupation:`writer`,
+    startingPrice:`30`
+
+  },
+  {
+    name: `Bob`,
+    occupation:`teacher`,
+    startingPrice:`50`
+  }
+];
+
+
+
+
+
+
